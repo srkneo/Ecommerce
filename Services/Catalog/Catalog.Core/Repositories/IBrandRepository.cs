cb48e6244a3149a55c9bc45113dea7baa0221550
yadav <yadav@CA>
2025-06-04 23:09:05 +0530

    Prodcut Repository created

---
+++ Services/Catalog/Catalog.Core/Repositories/IBrandRepository.cs
@@ -0,0 +1,15 @@
+﻿using Catalog.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Core.Repositories
+{
+    internal interface IBrandRepository
+    {
+        Task<IEnumerable<ProductBrand>> GetAllBrands();
+        
+    }
+}
---
+++ Services/Catalog/Catalog.Core/Repositories/IProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using Catalog.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Core.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProducts();
+        Task<Product> GetProduct(string id);
+        Task<IEnumerable<Product>> GetProductsByBrand(string name);
+        Task<IEnumerable<Product>> GetProductsByName(string name);
+        Task CreateProduct(Product product);
+        Task UpdateProduct(Product product);
+        Task DeleteProduct(string id);
+    }
+}
