fe2a11ab16b4b1f10c3b9f33a7a6dc5aa6616fea
yadav <yadav@CA>
2025-06-04 22:41:09 +0530

    created entities classes

---
+++ Services/Catalog/Catalog.Core/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Catalog.Core.Entities
+{
+    public class Product : BaseEntity
+    {
+        [BsonElement]
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Summary { get; set; } = string.Empty;
+        public ProductBrand Brands { get; set; }
+        public ProductType Types { get; set; }
+        public string ImageFile { get; set; } = string.Empty;
+
+        [BsonRepresentation(BsonType.Decimal128)]
+        public decimal Price { get; set; }
+
+    }
+}
