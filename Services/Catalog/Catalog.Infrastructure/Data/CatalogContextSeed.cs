7012eade66e5e2239cfbe5009eeb80980775a077
yadav <yadav@CA>
2025-06-04 23:44:26 +0530

    created otther context files

---
+++ Services/Catalog/Catalog.Infrastructure/Data/CatalogContextSeed.cs
@@ -0,0 +1,31 @@
+﻿using Catalog.Core.Entities;
+using MongoDB.Driver;
+using System.Text.Json;
+
+namespace Catalog.Infrastructure.Data
+{
+    public static class CatalogContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> productCollection)
+        {
+            bool checkProduct = productCollection.Find(p => true).Any();
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "Data", "SeedData", "products.json");
+            if (!checkProduct)
+            {
+                var productsData = File.ReadAllText(path);
+                var products = JsonSerializer.Deserialize<List<Product>>(productsData);
+                if (products != null && products.Any())
+                {
+                    foreach (var product in products)
+                    {
+                        productCollection.InsertOneAsync(product);
+                    }
+                }
+            }
+            else 
+            {
+                Console.WriteLine("Products already exist in the collection.");
+            }
+        }
+    }
+}
---
+++ Services/Catalog/Catalog.Infrastructure/Data/TypeContextSeed.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json;
+using Catalog.Core.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.Infrastructure.Data
+{
+    public static class TypeContextSeed
+    {
+        public static void SeedData(IMongoCollection<ProductType> typeCollection)
+        {
+            bool checkType = typeCollection.Find(t => true).Any();
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "Data", "SeedData", "types.json");
+            if (!checkType)
+            {
+                var typesData = File.ReadAllText(path);
+                var types = JsonSerializer.Deserialize<List<ProductType>>(typesData);
+                if (types != null && types.Any())
+                {
+                    foreach (var type in types)
+                    {
+                        typeCollection.InsertOneAsync(type);
+                    }
+                }
+            }
+        }
+    }
+}
