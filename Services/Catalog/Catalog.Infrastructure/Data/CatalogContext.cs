e5a28cac2e1c9501f246cdec9dc705801cfa5cd6
yadav <yadav@CA>
2025-06-05 10:26:29 +0530

    2-32

---
+++ Services/Catalog/Catalog.Infrastructure/Data/CatalogContext.cs
@@ -0,0 +1,30 @@
+﻿using Catalog.Core.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Infrastructure.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public IMongoCollection<Product> Products { get; }
+
+        public IMongoCollection<ProductBrand> Brands { get; }
+
+        public IMongoCollection<ProductType> Types { get; }
+
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var database = client.GetDatabase("CatalogDb");
+
+            Products = database.GetCollection<Product>("Products");
+            Brands = database.GetCollection<ProductBrand>("Brands");
+            Types = database.GetCollection<ProductType>("Types");
+        }
+    }
+}
