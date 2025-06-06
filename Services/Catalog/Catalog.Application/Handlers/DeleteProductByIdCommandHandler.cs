d328ab52ab10ec77c879f0f8feebe669e3b16016
max yad <yadavmaheshing@gmail.com>
2025-06-06 11:20:32 +0530

    2-46

---
+++ Services/Catalog/Catalog.Application/Handlers/DeleteProductByIdCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using Catalog.Application.Commands;
+using Catalog.Core.Repositories;
+using MediatR;
+
+namespace Catalog.Application.Handlers
+{
+    public class DeleteProductByIdCommandHandler : IRequestHandler<DeleteProductByIdCommand, bool>
+    {
+        private readonly IProductRepository _productRepository;
+
+        public DeleteProductByIdCommandHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public Task<bool> Handle(DeleteProductByIdCommand request, CancellationToken cancellationToken)
+        {
+            return _productRepository.DeleteProduct(request.Id);
+        }
+    }
+}
