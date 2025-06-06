db6cb66e812b0940624bad6c8422ec2af753687c
yadav <yadav@CA>
2025-06-06 08:20:26 +0530

    create handler

---
+++ Services/Catalog/Catalog.Application/Queries/GetProductByIdQuery.cs
@@ -0,0 +1,14 @@
+﻿using Catalog.Application.Responses;
+using MediatR;
+
+namespace Catalog.Application.Queries
+{
+    public class GetProductByIdQuery : IRequest<ProductResponse>
+    {
+        public string Id { get; }
+        public GetProductByIdQuery(string id)
+        {
+            Id = id;
+        }
+    }
+}
