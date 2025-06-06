e0bcc6109f93c2a6ee10263bf37be39fb97b45e9
max yad <yadavmaheshing@gmail.com>
2025-06-06 08:46:06 +0530

    2-44

---
+++ Services/Catalog/Catalog.Application/Handlers/GetProductByBrandHandler.cs
@@ -0,0 +1,23 @@
+﻿using Catalog.Application.Mappers;
+using Catalog.Application.Queries;
+using Catalog.Application.Responses;
+using Catalog.Core.Repositories;
+
+namespace Catalog.Application.Handlers
+{
+    public class GetProductByBrandHandler
+    {
+        private readonly IProductRepository _productRepository;
+        public GetProductByBrandHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+        public async Task<IList<ProductResponse>> Handle(GetProductByBrandQuery request, CancellationToken cancellationToken)
+        {
+            var products = await _productRepository.GetProductsByBrand(request.BrandName);
+            var productsResponseList = ProductMapper.Mapper.Map<IList<ProductResponse>>(products);
+
+            return productsResponseList;
+        }
+    }
+}
