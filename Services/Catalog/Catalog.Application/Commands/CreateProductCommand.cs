e1065c9c93569c9a6b6aed95c55a0a91bf52f7aa
max yad <yadavmaheshing@gmail.com>
2025-06-06 09:02:45 +0530

    2-44

---
+++ Services/Catalog/Catalog.Application/Commands/CreateProductCommand.cs
@@ -0,0 +1,17 @@
+﻿using Catalog.Application.Responses;
+using Catalog.Core.Entities;
+using MediatR;
+
+namespace Catalog.Application.Commands
+{
+    public class CreateProductCommand : IRequest<ProductResponse>
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Summary { get; set; } = string.Empty;
+        public ProductBrand Brands { get; set; }
+        public ProductType Types { get; set; }
+        public string ImageFile { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+    }
+}
---
+++ Services/Catalog/Catalog.Application/Handlers/CreateProductCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using Catalog.Application.Commands;
+using Catalog.Application.Mappers;
+using Catalog.Application.Responses;
+using Catalog.Core.Entities;
+using Catalog.Core.Repositories;
+using MediatR;
+
+namespace Catalog.Application.Handlers
+{
+    public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, ProductResponse>
+    {
+        private readonly IProductRepository _productRepository;
+
+        public CreateProductCommandHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<ProductResponse> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            var productEntity = ProductMapper.Mapper.Map<Product>(request);
+            if (productEntity is null) {
+                throw new ArgumentNullException(nameof(productEntity), "Product entity cannot be null");
+            }
+
+            var createdProduct = await _productRepository.CreateProduct(productEntity);
+            var ProductResponse = ProductMapper.Mapper.Map<ProductResponse>(createdProduct);
+            return ProductResponse;
+
+        }
+    }
+}
