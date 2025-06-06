ff908e3fe76fed4d6936d2fb8278501720095d4d
max yad <yadavmaheshing@gmail.com>
2025-06-06 08:33:10 +0530

    2-42

---
+++ Services/Catalog/Catalog.Application/Handlers/GetProductByNameQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿
+using Catalog.Application.Mappers;
+using Catalog.Application.Queries;
+using Catalog.Application.Responses;
+using Catalog.Core.Repositories;
+using MediatR;
+
+namespace Catalog.Application.Handlers
+{
+    public class GetProductByNameQueryHandler : IRequestHandler<GetProductByNameQuery, IList<ProductResponse>>
+    {
+        private readonly IProductRepository _productRepository;
+
+        public GetProductByNameQueryHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<IList<ProductResponse>> Handle(GetProductByNameQuery request, CancellationToken cancellationToken)
+        {
+            var products = await _productRepository.GetProductsByName(request.Name);
+            var productsResponse = ProductMapper.Mapper.Map<IList<ProductResponse>>(products);
+
+            return productsResponse;
+        }
+    }    
+}
