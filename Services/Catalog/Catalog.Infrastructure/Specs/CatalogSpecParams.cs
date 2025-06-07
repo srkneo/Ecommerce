cea249499ca6f377acda84207ed7e70060838c94
yadav <yadav@CA>
2025-06-07 14:39:03 +0530

    2-59

---
+++ Services/Catalog/Catalog.Infrastructure/Specs/CatalogSpecParams.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Catalog.Infrastructure.Specs
+{
+    public class CatalogSpecParams
+    {
+        private const int MaxPageSize = 70;
+        private int _pageSize;
+
+        public int PageSize
+        {
+            get => _pageSize; 
+            set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value; 
+        }
+
+        public string? BrandId { get; set; }
+        public string? TypeId { get; set; }
+        public string? Sort { get; set; }
+        public string? Search { get; set; }
+    }
+}
