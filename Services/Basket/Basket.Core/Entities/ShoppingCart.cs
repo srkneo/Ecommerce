d7ff197f234dea1e7a0bc3ff782890e270c00494
max yad <yadavmaheshing@gmail.com>
2025-06-09 14:16:32 +0530

    3-68

---
+++ Services/Basket/Basket.Core/Entities/ShoppingCart.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Basket.Core.Entities
+{
+    public class ShoppingCart
+    {
+
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
+        public decimal TotalPrice => Items.Sum(item => item.Price * item.Quantity);
+        public ShoppingCart() { }
+        public ShoppingCart(string userName)
+        {
+            UserName = userName;
+        }
+    }
+}
---
+++ Services/Basket/Basket.Core/Entities/ShoppingCartItem.cs
@@ -0,0 +1,12 @@
+﻿namespace Basket.Core.Entities
+{
+    public class ShoppingCartItem
+    {
+        public string Id { get; set; }
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public string ImageFile { get; set; }        
+    }
+}
