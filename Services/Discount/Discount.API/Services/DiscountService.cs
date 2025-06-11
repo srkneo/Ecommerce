3d0ef20fa969285f47c15c3f11ba05e84e756f0d
yadav <yadav@CA>
2025-06-11 22:04:27 +0530

    106

--- Services/Discount/Discount.API/Services/DiscountService.cs
+++ Services/Discount/Discount.API/Services/DiscountService.cs
@@ -1,6 +1,59 @@
-﻿namespace Discount.API.Services
+﻿using Discount.Application.Commands;
+using Discount.Application.Queries;
+using Discount.Grpc.Protos;
+using Grpc.Core;
+using MediatR;
+
+namespace Discount.API.Services
 {
-    public class DiscountService
+    public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
     {
+        private readonly IMediator _mediator;
+
+        public DiscountService(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        {
+            var query = new GetDiscountQuery(request.ProductName);
+            var coupon = await _mediator.Send(query);
+            return coupon;
+
+        }
+        // Additional methods for creating, updating, and deleting discounts can be added here
+
+        public override async Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+        {
+            var command = new CreateDiscountCommand
+            {
+                ProductName = request.Coupon.ProductName,
+                Description = request.Coupon.Description,
+                Amount = request.Coupon.Amount
+            };
+            var coupon = await _mediator.Send(command);
+            return coupon;
+        }
+
+        public override async Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+        {
+            var command = new UpdateDiscountCommand
+            {
+                Id = request.Coupon.Id,
+                ProductName = request.Coupon.ProductName,
+                Description = request.Coupon.Description,
+                Amount = request.Coupon.Amount
+            };
+            var coupon = await _mediator.Send(command);
+            return coupon;
+        }
+
+        public override async Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+        {
+            var command = new DeleteDiscountCommand (request.ProductName );
+            var result = await _mediator.Send(command);
+            return new DeleteDiscountResponse { Success = result };
+        }
     }
 }
