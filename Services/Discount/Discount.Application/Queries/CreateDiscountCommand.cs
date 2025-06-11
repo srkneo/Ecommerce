a60f1e8f4222dcadc8d341d7f54ce9a38aef00f3
max yad <yadavmaheshing@gmail.com>
2025-06-11 15:05:26 +0530

    4-104

---
+++ Services/Discount/Discount.Application/Queries/CreateDiscountCommand.cs
@@ -0,0 +1,12 @@
+﻿using Discount.Grpc.Protos;
+using MediatR;
+
+namespace Discount.Application.Queries
+{
+    public class CreateDiscountCommand : IRequest<CouponModel>
+    {
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public int Amount { get; set; }
+    }
+}
