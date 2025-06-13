c51c4f42a50018c8a1610b0a3d9ca18ae5fa64e8
max yad <yadavmaheshing@gmail.com>
2025-06-13 06:54:46 +0530

    134

---
+++ Services/Ordering/Ordering.Infrastructure/Data/OrderContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Core.Common;
+using Ordering.Core.Entities;
+
+namespace Ordering.Infrastructure.Data
+{
+    public class OrderContext : DbContext
+    {
+        public OrderContext(DbContextOptions<OrderContext> options) : base(options)
+        {
+        }        
+        public DbSet<Order> Orders { get; set; }
+
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<EntityBase>())
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    // Set default values or perform any additional logic for new orders
+                    entry.Entity.CreatedDate = DateTime.Now;
+                    entry.Entity.CreatedBy = "Mahesh"; 
+                }
+                else if (entry.State == EntityState.Modified)
+                {
+                    entry.Entity.LastModifiedDate = DateTime.Now;
+                    entry.Entity.LastModifiedBy = "Mahesh";                    
+                }
+                
+            }
+            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        }
+
+    }
+}
---
+++ Services/Ordering/Ordering.Infrastructure/Data/OrderContextSeed.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Core.Entities;
+
+namespace Ordering.Infrastructure.Data
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext orderContext, ILogger<OrderContextSeed> logger)
+        {
+            try
+            {
+                if (!orderContext.Orders.Any())
+                {
+                    orderContext.Orders.AddRange(GetPreconfiguredOrders());
+                    await orderContext.SaveChangesAsync();
+                    logger.LogInformation($"Seeded database with initial data.{typeof(OrderContext).Name}");
+                }
+                else
+                {
+                    logger.LogInformation("Database already seeded with data.");
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.LogError($"An error occurred while seeding the database: {ex.Message}");
+            }
+        }
+
+        private static IEnumerable<Order> GetPreconfiguredOrders()
+        {
+            return new List<Order>
+        {
+            new()
+            {
+                UserName = "rahul",
+                FirstName = "Rahul",
+                LastName = "Sahay",
+                EmailAddress = "rahulsahay@eshop.net",
+                AddressLine = "Bangalore",
+                Country = "India",
+                TotalPrice = 750,
+                State = "KA",
+                ZipCode = "560001",
+
+                CardName = "Visa",
+                CardNumber = "1234567890123456",
+                CreatedBy = "Rahul",
+                Expiration = "12/25",
+                Cvv = "123",
+                PaymentMethod = 1,
+                LastModifiedBy = "Rahul",
+                LastModifiedDate = new DateTime(),
+            }
+        };
+        }
+    }
+}
