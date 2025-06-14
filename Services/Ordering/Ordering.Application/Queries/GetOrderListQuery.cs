3c74945e45e72bcc6b4b7f3b261a9ea6a99be052
max yad <yadavmaheshing@gmail.com>
2025-06-14 11:42:42 +0530

    142

---
+++ Services/Ordering/Ordering.Application/Queries/GetOrderListQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Ordering.Application.Responses;
+
+namespace Ordering.Application.Queries
+{
+    public class GetOrderListQuery : IRequest<List<OrderResponse>>
+    {
+        public string UserName { get; set; }
+
+        public GetOrderListQuery(string userName)
+        {
+            UserName = userName;
+        }
+    }
+}
