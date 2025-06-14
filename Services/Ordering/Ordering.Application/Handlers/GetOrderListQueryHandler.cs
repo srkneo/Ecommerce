c9b5eda18dd698c9a854d4a08d4c745f4fd6b1a6
max yad <yadavmaheshing@gmail.com>
2025-06-14 11:49:05 +0530

    143

---
+++ Services/Ordering/Ordering.Application/Handlers/GetOrderListQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using MediatR;
+using Ordering.Application.Queries;
+using Ordering.Application.Responses;
+using Ordering.Core.Entities;
+using Ordering.Core.Repositories;
+
+namespace Ordering.Application.Handlers
+{
+    public class GetOrderListQueryHandler : IRequestHandler<GetOrderListQuery, List<OrderResponse>>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
+        public GetOrderListQueryHandler(IOrderRepository orderRepository,IMapper mapper)
+        {
+            _orderRepository = orderRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<OrderResponse>> Handle(GetOrderListQuery request, CancellationToken cancellationToken)
+        {
+            var order = await _orderRepository.GetOrdersByUserName(request.UserName);
+
+            return  _mapper.Map<List<OrderResponse>>(order);
+
+        }
+    }
+}
