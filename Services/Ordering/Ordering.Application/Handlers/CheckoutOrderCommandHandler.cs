93e9797af995de904f8f9d7d8f91c9724469e819
max yad <yadavmaheshing@gmail.com>
2025-06-14 16:12:23 +0530

    145

---
+++ Services/Ordering/Ordering.Application/Handlers/CheckoutOrderCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Commands;
+using Ordering.Core.Entities;
+using Ordering.Core.Repositories;
+using System.Collections.Specialized;
+
+namespace Ordering.Application.Handlers
+{
+    public class CheckoutOrderCommandHandler : IRequestHandler<CheckoutOrderCommand, int>
+    {
+
+        private readonly IOrderRepository _orderedRepository;
+        private readonly IMapper _mapper;
+        private readonly ILogger _logger;
+
+
+        public CheckoutOrderCommandHandler(IOrderRepository orderedRepository, IMapper mapper, ILogger logger)
+        {
+
+            _orderedRepository = orderedRepository;
+            _mapper = mapper;
+            _logger = logger;
+        }
+
+        public async Task<int> Handle(CheckoutOrderCommand request, CancellationToken cancellationToken)
+        {
+            var orderEntity = _mapper.Map<Order>(request);
+            var genratedOrder = await _orderedRepository.AddAsync(orderEntity);
+            _logger.LogInformation($"Order with id {genratedOrder.Id} is successfully created.");
+
+            return genratedOrder.Id;
+        }
+    }
+}
