77da74993e3c12f1e20f4555fa693db11bb859ef
max yad <yadavmaheshing@gmail.com>
2025-06-16 06:38:48 +0530

    151

--- Services/Ordering/Ordering.Application/Validators/CheckoutOrderCommandValidator.cs
+++ Services/Ordering/Ordering.Application/Validators/CheckoutOrderCommandValidator.cs
@@ -1,12 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FluentValidation;
+using Ordering.Application.Commands;
 
 namespace Ordering.Application.Validators
 {
-    internal class CheckoutOrderCommandValidator
+    public class CheckoutOrderCommandValidator : AbstractValidator<CheckoutOrderCommand>
     {
+        public CheckoutOrderCommandValidator()
+        {
+            RuleFor(x => x.UserName)
+                .NotEmpty().WithMessage("Username is required.")
+                .MaximumLength(50).WithMessage("Username must not exceed 50 characters.");
+
+            RuleFor(x => x.TotalPrice)
+                .NotNull().WithMessage("Total price is required.")
+                .GreaterThan(0).WithMessage("Total price must be greater than zero.");
+
+            RuleFor(x => x.EmailAddress)
+                .NotEmpty().WithMessage("Email address is required.")
+                .EmailAddress().WithMessage("Invalid email address format.");
+
+            RuleFor(x => x.FirstName)
+                .NotEmpty().WithMessage("First name is required.")
+                .MaximumLength(50).WithMessage("First name must not exceed 50 characters.");
+
+            RuleFor(x => x.LastName)
+                .NotEmpty().WithMessage("Last name is required.")
+                .MaximumLength(50).WithMessage("Last name must not exceed 50 characters.");
+        }
+
     }
 }
