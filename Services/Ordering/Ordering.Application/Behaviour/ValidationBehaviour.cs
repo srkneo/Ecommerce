0565f12b211f0c19be6fcfd45dec1272e58f4f96
max yad <yadavmaheshing@gmail.com>
2025-06-16 07:01:40 +0530

    154

---
+++ Services/Ordering/Ordering.Application/Behaviour/ValidationBehaviour.cs
@@ -0,0 +1,43 @@
+﻿using FluentValidation;
+using MediatR;
+
+namespace Ordering.Application.Behaviour
+{
+    public class ValidationBehaviour<Treqeust, Tresponse> : IPipelineBehavior<Treqeust, Tresponse>
+    where Treqeust : IRequest<Tresponse>
+    {
+        private readonly IEnumerable<IValidator<Treqeust>> _validators;
+        public ValidationBehaviour( IEnumerable<IValidator<Treqeust>> validators )
+        {
+            _validators = validators;
+        }
+
+        public async Task<Tresponse> Handle(Treqeust request, RequestHandlerDelegate<Tresponse> next, CancellationToken cancellationToken)
+        {
+            if (_validators.Any()) 
+            {
+                var context = new ValidationContext<Treqeust>(request);
+
+                //This will run all the validators against the request
+                var validationResults = await Task.WhenAll(
+                    _validators
+                    .Select(v => v.ValidateAsync(context, cancellationToken))
+                    );
+
+                //If any of the validators have errors, throw an exception
+                var failures = validationResults
+                    .SelectMany(result => result.Errors)
+                    .Where(f => f != null)
+                    .ToList();
+
+                if (failures.Count != 0)
+                {
+                    throw new ValidationException(failures);
+                }
+            }
+
+            // Validate the request using the validators
+            return await next();
+        }
+    }
+}
