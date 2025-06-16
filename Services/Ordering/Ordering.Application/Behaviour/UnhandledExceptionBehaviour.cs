0397f13fc55613dcf0da92b8b1943f05f06f4a2c
max yad <yadavmaheshing@gmail.com>
2025-06-16 07:07:13 +0530

    155

---
+++ Services/Ordering/Ordering.Application/Behaviour/UnhandledExceptionBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Ordering.Application.Behaviour
+{
+    public class UnhandledExceptionBehaviour<Trequest, Tresponse> : IPipelineBehavior<Trequest, Tresponse>
+    where Trequest : IRequest<Tresponse>
+    {
+        private readonly ILogger<Trequest> _logger;
+        public UnhandledExceptionBehaviour(ILogger<Trequest> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<Tresponse> Handle(Trequest request, RequestHandlerDelegate<Tresponse> next, CancellationToken cancellationToken)
+        {
+            try
+            {
+                return await next();
+            }
+            catch (Exception ex)
+            {
+                var requestName = typeof(Trequest).Name;
+                _logger.LogError(ex, "Unhandled exception occured with Request: {Name} {@Request}", requestName, request);
+                throw; // Re-throw the exception to ensure it propagates
+            }
+        }
+    }
+}
