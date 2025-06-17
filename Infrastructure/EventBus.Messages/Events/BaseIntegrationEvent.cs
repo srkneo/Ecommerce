5cc65538e335a7703380b05446b92af9a00fe28b
yadav <yadav@CA>
2025-06-17 06:14:49 +0530

    173

---
+++ Infrastructure/EventBus.Messages/Events/BaseIntegrationEvent.cs
@@ -0,0 +1,20 @@
+﻿namespace EventBus.Messages.Events
+{
+    public class BaseIntegrationEvent
+    {
+        public string CorrelationId { get; set; }
+        public DateTime CreationDate { get; private set; }
+
+        public BaseIntegrationEvent()
+        {
+            CorrelationId = Guid.NewGuid().ToString();
+            CreationDate = DateTime.UtcNow;
+        }
+
+        public BaseIntegrationEvent(Guid corelationId,DateTime creationData)
+        {
+            CorrelationId = corelationId.ToString();
+            CreationDate = creationData;
+        }
+    }
+}
