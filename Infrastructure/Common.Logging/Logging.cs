7d7e0c1f3031bd2049c8f4249b5c7a6bf88e668b
yadav <yadav@CA>
2025-06-18 11:08:54 +0530

    187

---
+++ Infrastructure/Common.Logging/Logging.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Exceptions;
+
+namespace Common.Logging
+{
+    public static class Logging
+    {
+        public static Action<HostBuilderContext, LoggerConfiguration> cfg => 
+            (context, loggerConfiguration) =>
+            {
+                var environment = context.HostingEnvironment;
+                loggerConfiguration
+                    .MinimumLevel.Information()
+                    .Enrich.FromLogContext()
+                    .Enrich.WithProperty("EnvironmentName", environment.EnvironmentName)
+                    .Enrich.WithProperty("ApplicationName", environment.ApplicationName)
+                    .Enrich.WithExceptionDetails()
+                    .MinimumLevel.Override("Microsoft.AspNetCore", Serilog.Events.LogEventLevel.Warning)
+                    .MinimumLevel.Override("Microsoft.Hosting.Lifetime", Serilog.Events.LogEventLevel.Warning)
+                    .WriteTo.Console();
+
+                if (environment.IsDevelopment())
+                {
+                    loggerConfiguration.MinimumLevel.Override("Catalog", Serilog.Events.LogEventLevel.Debug);
+                    loggerConfiguration.MinimumLevel.Override("Basket", Serilog.Events.LogEventLevel.Debug);
+                    loggerConfiguration.MinimumLevel.Override("Discount", Serilog.Events.LogEventLevel.Debug);
+                    loggerConfiguration.MinimumLevel.Override("Ordering", Serilog.Events.LogEventLevel.Debug);
+
+                }
+
+            };
+    }
+}
