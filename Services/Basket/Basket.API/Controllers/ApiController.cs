ceb0a4f77f075824159e35315a6dc2662462f15d
max yad <yadavmaheshing@gmail.com>
2025-06-19 12:04:02 +0530

    211

--- Services/Basket/Basket.API/Controllers/ApiController.cs
+++ Services/Basket/Basket.API/Controllers/ApiController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Basket.API.Controllers
 {
-    [ApiVersion("1")]
+    
     [Route("api/v{version:ApiVersion}/[controller]")]
     [ApiController]
     public class ApiController : ControllerBase
