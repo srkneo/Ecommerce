23a041bddb4a297d74a1addc6510cdb8cc6e257f
max yad <yadavmaheshing@gmail.com>
2025-06-19 11:32:50 +0530

    206

---
+++ Services/Basket/Basket.API/Controllers/V2/BasketController.cs
@@ -0,0 +1,53 @@
+﻿using Asp.Versioning;
+using Basket.Application.Commands;
+using Basket.Application.Mappers;
+using Basket.Application.Queries;
+using Basket.Core.Entities;
+using EventBus.Messages.Common;
+using MassTransit;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Basket.API.Controllers.V2
+{
+    [ApiVersion("2")]
+    [Route("api/v{version:ApiVersion}/[controller]")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly IPublishEndpoint _publishEndpoint;
+        private readonly ILogger<BasketController> _logger;
+
+        public BasketController(IMediator mediator, IPublishEndpoint publishEndpoint, ILogger<BasketController> logger)
+        {
+            _mediator = mediator;
+            _publishEndpoint = publishEndpoint;
+            _logger = logger;
+        }
+
+        [Route("[action]")]
+        [HttpPost]
+        [ProducesResponseType((int)HttpStatusCode.Accepted)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<IActionResult> Checkout([FromBody] BasketCheckoutV2 basketCheckout)
+        {
+            // Get the basket by username
+            var basket = await _mediator.Send(new GetBasketByUserNameQuery(basketCheckout.UserName));
+            if (basket == null)
+            {
+                return BadRequest("Basket not found.");
+            }
+            var eventMsg = BasketMapper.Mapper.Map<BasketCheckoutEventV2>(basketCheckout);
+            await _publishEndpoint.Publish(eventMsg);
+
+            _logger.LogInformation("BasketCheckoutEvent published successfully for user with V2: {UserName}", basket.UserName);
+
+            // Remove the basket after checkout
+            await _mediator.Send(new DeleteBasketByUserNameCommand(basketCheckout.UserName));
+
+            return Accepted();
+        }
+    }
+}
