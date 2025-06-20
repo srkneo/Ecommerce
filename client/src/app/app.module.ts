6c771c59ca6db9b5c4e98f01a6a7f8f60ce77b64
max yad <yadavmaheshing@gmail.com>
2025-06-20 14:31:45 +0530

    450

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations'; // Importing BrowserAnimationsModule is optional, depending on your needs
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent    
+  ],
   imports: [BrowserModule, 
     AppRoutingModule, 
-    BrowserAnimationsModule],
+    BrowserAnimationsModule,NavbarComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
