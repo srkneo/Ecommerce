ca6223054e2bcf71b1ae958dbbf4f56fba9549fa
max yad <yadavmaheshing@gmail.com>
2025-06-21 11:20:21 +0530

    need gateway

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -6,6 +7,19 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  title = 'eShopping';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    // Example of making an HTTP request
+    this.http.get('http://localhost:8000/api/v1/Catalog/GetProductByProductName/Adidas%20FIFA%20World%20Cup%202018%20OMB%20Football').subscribe({
+
+      next: response => console.log(response),
+      error: error => console.error('There was an error!', error),
+      complete: () => console.log('HTTP request completed')
+
+    })
+  }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations'; // Importing BrowserAnimationsModule is optional, depending on your needs
 import { NavbarComponent } from './navbar/navbar.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [AppComponent    
-  ],
+  declarations: [AppComponent],
   imports: [BrowserModule, 
     AppRoutingModule, 
-    BrowserAnimationsModule,NavbarComponent],
+    BrowserAnimationsModule,
+    HttpClientModule,
+    NavbarComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
