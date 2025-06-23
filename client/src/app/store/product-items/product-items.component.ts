a63106c74309198119888ef126e48db22eb117de
max yad <yadavmaheshing@gmail.com>
2025-06-23 14:09:53 +0530

    461

---
+++ client/src/app/store/product-items/product-items.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-items',
+  templateUrl: './product-items.component.html',
+  styleUrl: './product-items.component.scss'
+})
+export class ProductItemsComponent {
+
+}
--- client/src/app/store/store.module.ts
+++ client/src/app/store/store.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StoreComponent } from './store.component';
+import { ProductItemsComponent } from './product-items/product-items.component';
 
 
 
 @NgModule({
-  declarations: [StoreComponent],
+  declarations: [StoreComponent,ProductItemsComponent],
   imports: [
-    CommonModule
+    CommonModule    
   ],
-  exports: [StoreComponent] // Export StoreComponent so it can be used in other modules
+  exports: [StoreComponent,ProductItemsComponent] // Export StoreComponent so it can be used in other modules
 })
 export class StoreModule { }
