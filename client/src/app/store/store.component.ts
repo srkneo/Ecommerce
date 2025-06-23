7ce764312957f19658624614195897b547ab2ba8
yadav <yadav@CA>
2025-06-23 23:50:38 +0530

    466

--- client/src/app/store/store.component.ts
+++ client/src/app/store/store.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { StoreService } from './store.service';
 import { IProduct } from '../shared/models/product';
+import { IBrand } from '../shared/models/brand';
+import { IType } from '../shared/models/type';
 
 @Component({
   selector: 'app-store',
@@ -8,17 +10,44 @@ import { IProduct } from '../shared/models/product';
   styleUrl: './store.component.scss'
 })
 export class StoreComponent implements OnInit {
-  products: IProduct[] = []; // Replace 'any' with the actual type of your products  
+  products: IProduct[] = []; 
+  brands: IBrand[] = [];
+  types: IType[] = []; 
   constructor(private storeService:StoreService) { // Replace 'any' with the actual type of your store service
     // Initialization logic can go here
   }
   ngOnInit(): void {
-    this.storeService.getProducts().subscribe({
+   this.getProducts();
+   this.getBrands();
+   this.getTypes();
+  }
 
-      next: response => this.products = response.data // Assign the fetched products to the component's products property
-      , error: error => console.error('Error fetching products:', error) // Handle any errors that occur during the fetch
-      , complete: () => console.log('Product fetch complete') // Optional: Log when the
-      
-    }); // Fetch products from the store service
+  getProducts(){
+      this.storeService.getProducts().subscribe({
+
+        next: response => this.products = response.data // Assign the fetched products to the component's products property
+        , error: error => console.error('Error fetching products:', error) // Handle any errors that occur during the fetch
+        , complete: () => console.log('Product fetch complete') // Optional: Log when the
+        
+      }); 
+  }
+  getBrands(){
+      this.storeService.getBrands().subscribe({
+
+        next: response => this.brands = response // Assign the fetched products to the component's products property
+        , error: error => console.error('Error fetching products:', error) // Handle any errors that occur during the fetch
+        , complete: () => console.log('Product fetch complete') // Optional: Log when the
+        
+      }); 
+  }
+
+  getTypes(){
+      this.storeService.getTypes().subscribe({
+
+        next: response => this.types = response // Assign the fetched products to the component's products property
+        , error: error => console.error('Error fetching products:', error) // Handle any errors that occur during the fetch
+        , complete: () => console.log('Product fetch complete') // Optional: Log when the
+        
+      }); 
   }
 }
--- client/src/app/store/store.service.ts
+++ client/src/app/store/store.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { IPagination } from '../shared/models/pagination';
 import { IProduct } from '../shared/models/product';
+import { IBrand } from '../shared/models/brand';
+import { IType } from '../shared/models/type';
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +15,14 @@ export class StoreService {
 
   getProducts() {
     return this.http.get<IPagination<IProduct[]>>(this.baseUrl + 'Catalog/GetAllProducts');    
+  }
+
+  getBrands(){
+    return this.http.get<IBrand[]>(this.baseUrl + 'Catalog/GetAllBrands');
+  }
+
+  getTypes(){
+    return this.http.get<IType[]>(this.baseUrl + 'Catalog/GetAllTypes');
   }
 
 }
