f36dac416bc23882333185872bcce7c7d7751771
max yad <yadavmaheshing@gmail.com>
2025-06-23 11:10:23 +0530

    459

--- client/src/app/store/store.service.ts
+++ client/src/app/store/store.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { IPagination } from '../shared/models/pagination';
+import { IProduct } from '../shared/models/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StoreService {
 
-  constructor() { }
+  constructor(private http:HttpClient) {}
+  baseUrl = 'http://localhost:8010/';
+
+  getProducts() {
+    return this.http.get<IPagination<IProduct[]>>(this.baseUrl + 'Catalog/GetAllProducts');    
+  }
+
 }
