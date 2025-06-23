92a1ca6cc8303b952e6ac495782a330cfc240575
max yad <yadavmaheshing@gmail.com>
2025-06-23 11:04:23 +0530

    458

--- client/src/app/store/store.module.ts
+++ client/src/app/store/store.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { StoreComponent } from './store.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [StoreComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [StoreComponent] // Export StoreComponent so it can be used in other modules
 })
 export class StoreModule { }
