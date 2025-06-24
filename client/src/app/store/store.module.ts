ea88dc1d71cccd613c8af54fc8795dc9ff48edbe
max yad <yadavmaheshing@gmail.com>
2025-06-24 11:18:04 +0530

    476

--- client/src/app/store/store.module.ts
+++ client/src/app/store/store.module.ts
@@ -3,15 +3,16 @@ import { CommonModule } from '@angular/common';
 import { StoreComponent } from './store.component';
 import { ProductItemsComponent } from './product-items/product-items.component';
 import { SharedModule } from '../shared/shared.module';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 
 
 @NgModule({
-  declarations: [StoreComponent,ProductItemsComponent],
+  declarations: [StoreComponent,ProductItemsComponent,ProductDetailsComponent],
   imports: [
     CommonModule,
     SharedModule
   ],
-  exports: [StoreComponent,ProductItemsComponent] // Export StoreComponent so it can be used in other modules
+  exports: [StoreComponent,ProductItemsComponent,ProductDetailsComponent] // Export StoreComponent so it can be used in other modules
 })
 export class StoreModule { }
