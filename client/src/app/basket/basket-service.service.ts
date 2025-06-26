521f121e431c2e7816d2048d747f5e54a29fc31d
max yad <yadavmaheshing@gmail.com>
2025-06-26 19:41:19 +0530

    499

--- client/src/app/basket/basket-service.service.ts
+++ client/src/app/basket/basket-service.service.ts
@@ -1,9 +1,39 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Basket, IBasket } from '../shared/models/basket';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BasketServiceService {
 
-  constructor() { }
+  baseurl = 'https://localhost:9010';
+
+  constructor(private http: HttpClient) { }
+  private basketSource = new BehaviorSubject<Basket | null>(null);
+  baseketSource$ = this.basketSource.asObservable();
+
+  getBasket(username: string) {
+    return this.http.get<IBasket>(this.baseurl + '/Basket/GetBasket/mahesh').subscribe({
+      next: (basket) => {
+        this.basketSource.next(basket);
+      },
+      error: (error) => {
+        console.error('Error fetching basket:', error);
+      }
+    });
+  }
+
+  setbasket(basket: Basket) {
+    return this.http.post<IBasket>(this.baseurl + '/Basket/CreateBasket', basket).subscribe({
+      next: (basket) => { 
+        this.basketSource.next(basket);
+      }
+    });
+  }
+
+  getcurrentBasket() {
+    return this.basketSource.value;
+  }
 }
