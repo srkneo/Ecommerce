170857134325999a496284d074c391470af02783
max yad <yadavmaheshing@gmail.com>
2025-06-27 07:05:52 +0530

    502

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,8 +1,5 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { IPagination } from './shared/models/pagination';
-import { IProduct } from './shared/models/product';
+import { BasketService } from './basket/basket.service';
 
 @Component({
   selector: 'app-root',
@@ -12,22 +9,12 @@ import { IProduct } from './shared/models/product';
 export class AppComponent implements OnInit {
   title = 'eShopping';
 
-  products: any[] = [];
-
-  constructor(private http: HttpClient) {}
+  constructor(private basketService: BasketService) {}
 
   ngOnInit(): void {
-    // Example of making an HTTP request
-    this.http.get<IPagination<IProduct[]>>('http://localhost:8010/Catalog/GetAllProducts').subscribe({
-
-      next: response =>{
-        this.products = response.data;
-        console.log(response)
-
-      } ,
-      error: error => console.error('There was an error!', error),
-      complete: () => console.log('HTTP request completed')
-
-    })
+    const basket_username = localStorage.getItem('basket_username');
+    if (basket_username) {
+      this.basketService.getBasket(basket_username);
+    }   
   }
 }
